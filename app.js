758a3bb5f5d54414cfb22fcd27d1ebe4cc7821bb
Yan Linn Aung <yanlinnaung.dev@gmail.com>
2026-09-24 21:12:14 +0630

    comment feature one

--- app.js
+++ app.js
@@ -2,38 +2,38 @@ const todoInput = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const todolist = document.getElementById("todo-list");
 
-// Add Event Listener to the Add Button
-addBtn.addEventListener("click", function() {
-    const taskText = todoInput.value;
+// // Add Event Listener to the Add Button
+// addBtn.addEventListener("click", function() {
+//     const taskText = todoInput.value;
 
-    if(taskText === "") {
-        alert("Please enter a task!");
-        return;
-    }
+//     if(taskText === "") {
+//         alert("Please enter a task!");
+//         return;
+//     }
     
-    // Create new <li> element
-    const li = document.createElement("li");
-    li.textContent = taskText;
+//     // Create new <li> element
+//     const li = document.createElement("li");
+//     li.textContent = taskText;
 
-    // Toggle completed status of click
-    li.addEventListener("click", function() {
-        li.classList.toggle("completed");
-    })
+//     // Toggle completed status of click
+//     li.addEventListener("click", function() {
+//         li.classList.toggle("completed");
+//     })
 
-    // Create Delete Button
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "X";
-    deleteBtn.classList.add("delete-btn");
+//     // Create Delete Button
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.textContent = "X";
+//     deleteBtn.classList.add("delete-btn");
 
-    // Delete item on click
-    deleteBtn.addEventListener("click", function() {
-        li.remove();
-    })
+//     // Delete item on click
+//     deleteBtn.addEventListener("click", function() {
+//         li.remove();
+//     })
 
-    // Appen delete button to li
-    li.appendChild(deleteBtn);
-    todolist.appendChild(li);
+//     // Appen delete button to li
+//     li.appendChild(deleteBtn);
+//     todolist.appendChild(li);
 
-    // Clear input field
-    todoInput.value = "";
-})
+//     // Clear input field
+//     todoInput.value = "";
+// })
